14366299f66ce081317923d13834ea61cd2cdb55
SamanSah <sahraei.saman91@gmail.com>
2023-06-24 18:58:10 +0330

    compeleted DB calss

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs, { writeFileSync } from 'fs'
 import chalk from 'chalk';
 
 const filename= process.env.DB_FILE;
@@ -89,4 +89,125 @@ export default class DB {
             throw new Error("Syntax error.\nPlease check the DB file")
         }
     }
+
+    static saveTask(title, completed= false, id= 0) {
+        id= Number(id);
+        if(id < 0 || id!== parseInt(id)) {
+            throw new Error("id must be an integer, equal or greater than zero")
+        }else if(typeof title !== "string" || title.length < 3) {
+            throw new Error("title is required and should have at least three characters.")
+        }
+
+        const task= DB.getTaskByTitle(title);
+        if(task && task.id != id) {
+            throw new Error ("A task exists with this title")
+        }
+
+        let data;
+        if(DB.existsDB()) {
+            data= fs.readFileSync(filename, "utf-8");
+        }else {
+            try {
+                DB.createDB();
+                data= "[]"
+            } catch (error) {
+                throw new Error(error.message)
+            }
+        }
+
+        try {
+            data=JSON.parse(data);
+        } catch (error) {
+            throw new Error("Syntax error.\nPlease check the DB file")
+        }
+
+
+        if(id=== 0) {
+            if(data.length === 0){
+                id= 1;
+            } else {
+                id= data[data.length - 1].id + 1;
+            }
+            data.push({
+                id,
+                title,
+                completed
+            })
+            const str= JSON.stringify(data, null, "    ")
+            try {
+                fs.writeFileSync(filename, str, "utf-8")
+            } catch (error) {
+                throw new Error("Can not save the task")
+            }
+        }else {
+            for(let i=0; i< data.length; i++) {
+                if(data[i].id === id) {
+                    data[i].title= title;
+                    data[i].completed= completed;
+                    const str = JSON.stringify(data, null, "    ");
+                    try {
+                        fs.writeFileSync(filename, str, "utf-8");
+                        return true
+                    } catch (error) {
+                        throw new Error("Can not save the task");
+                    }
+                }
+            }
+            throw new Error("Task not Found")
+        }
+
+    }
+
+    static insertBulkData(data) {
+        if(typeof data === "string") {
+            try {
+                data= JSON.parse(data)
+            } catch (error) {
+                throw new Error("Invalid data")
+            }            
+        }
+        if(data instanceof Array) {
+            data= JSON.stringify(data, null, "    ")
+        }else {
+            throw new Error("Invalid data")
+        }
+
+        try {
+            fs,writeFileSync(filename, data)
+        } catch (error) {
+            throw new Error("Can not write to DB file.")
+        }
+    }
+
+    static deleteTaske(id) {
+        id= Number(id);
+        if(id > 0 && id === parseInt(id)) {
+            let data;
+            try{
+                data= fs.readFileSync(filename, "utf-8")
+                data= JSON.parse(data);
+            }catch(error) {
+                throw new Error("Can not read DB file.")            
+            }
+            for(let i=0; i < data.length; i++){
+                if(data[i].id === id) {
+                    data.splice(i, 1);
+                    data= JSON.stringify(data, null, "    ")
+                    try {
+                        fs.writeFileSync(filename, data, "utf-8")
+                        return true
+                    } catch (error) {
+                        throw new Error("Can not write in DB file.")  
+                    }
+                }
+            }
+            return false
+        }else {
+            throw new Error("Task id must be a positive integer")  
+        }
+    }
 }
+
+
+
+
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import "dotenv/config"
 import DB from "./db.js"
-
+console.clear();
 // DB.resetDB();
-console.log(DB.getAllTasks());
+// console.log(DB.getAllTasks());
+DB.saveTask("Learn NodeJs")
+// DB.deleteTaske(1)
